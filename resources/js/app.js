2082c503350b5d2fb110185515370ac6c99c6eaf
sugitakai <78083472+sugitakai@users.noreply.github.com>
2026-08-14 20:36:13 +0900

    能力値マトリクス表　css作

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,5 @@
 import './bootstrap';
 import { createApp } from 'vue';
 import MakeCharaSheet from './components/make-chara-sheet.vue';
-
+import '../css/chara-sheet.css';
 createApp(MakeCharaSheet).mount('#app');
