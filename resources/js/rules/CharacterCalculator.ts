060ed7910b8a2d3cdb0a300d10e24ab9e42c566d
sugitakai <78083472+sugitakai@users.noreply.github.com>
2026-08-18 21:39:30 +0900

    関数名直し

--- resources/js/rules/CharacterCalculator.ts
+++ resources/js/rules/CharacterCalculator.ts
@@ -4,23 +4,23 @@ export const assignedParameter= {
 	spell: null
     }
 
-export function calculatevitality() {
+export function calculateVitality() {
 	const base = abilities.strength + abilities.spirit + abilities.endurance;
 	const dice = assignedParameter.vitality || 0;
 	return base + dice;
 }
 
-export function calculatevitality2() {
+export function calculateVitality2() {
 	return vitality * 2;
 }
 
-export function calculatemove() {
+export function calculateMove() {
 	const dice = assignedParameter.move || 0;
 	const bonus = raceBonusTable[race]?.moveBonus || 1;
 	return dice * bonus;
 }
 
-export function calculatespellUses() {
+export function calculateSpellUses() {
 	const dice = assignedParameter.spell || 0;
 	let base = 0;
 	if (dice <= 6) base = 0;
@@ -30,6 +30,6 @@ export function calculatespellUses() {
 	return base + spellSkillBonus;
 }
 
-export function calculateresistBase() {
+export function calculateResistBase() {
 	return abilities.spirit + abilities.reflex + adventurerLevel + skillBonus;
 }
